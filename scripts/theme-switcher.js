0ae728e0b716b62f1e368ed7bccbe9d404087aae
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2022-05-11 01:53:02 -0400

    Update theme-switcher.js

--- scripts/theme-switcher.js
+++ scripts/theme-switcher.js
@@ -1,10 +1,15 @@
- 
-let systemInitiatedDark = window.matchMedia("(prefers-color-scheme: dark)");
+let systemInitiatedDark = window.matchMedia("(prefers-color-scheme: dark)"); 
 let theme = sessionStorage.getItem('theme');
+
+if (systemInitiatedDark.matches) {
+	document.getElementById("theme-toggle").innerHTML = "Light Mode";
+} else {
+	document.getElementById("theme-toggle").innerHTML = "Dark Mode";
+}
 
 function prefersColorTest(systemInitiatedDark) {
   if (systemInitiatedDark.matches) {
-  	document.documentElement.setAttribute('data-theme', 'dark');
+  	document.documentElement.setAttribute('data-theme', 'dark');		
    	document.getElementById("theme-toggle").innerHTML = "Light Mode";
    	sessionStorage.setItem('theme', '');
   } else {
@@ -13,14 +18,8 @@ function prefersColorTest(systemInitiatedDark) {
     sessionStorage.setItem('theme', '');
   }
 }
-
-if (systemInitiatedDark.matches) {
-	document.getElementById("theme-toggle").innerHTML = "Light Mode";
-} else {
-	document.getElementById("theme-toggle").innerHTML = "Dark Mode";
-}
-
 systemInitiatedDark.addListener(prefersColorTest);
+
 
 function modeSwitcher() {
 	let theme = sessionStorage.getItem('theme');
@@ -32,7 +31,7 @@ function modeSwitcher() {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
 		document.getElementById("theme-toggle").innerHTML = "Light Mode";
-	} else if (systemInitiatedDark.matches) {
+	} else if (systemInitiatedDark.matches) {	
 		document.documentElement.setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
 		document.getElementById("theme-toggle").innerHTML = "Dark Mode";
@@ -52,4 +51,3 @@ if (theme === "dark") {
 	sessionStorage.setItem('theme', 'light');
 	document.getElementById("theme-toggle").innerHTML = "Dark Mode";
 }
-
